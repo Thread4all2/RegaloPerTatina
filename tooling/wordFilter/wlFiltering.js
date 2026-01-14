6006e8bb1111b637066dde47a8eed3a91f49d6a2
Thread4all2 <ericme235@gmail.com>
2026-01-14 03:09:12 +0100

    wordlist refinement

--- tooling/wordFilter/wlFiltering.js
+++ tooling/wordFilter/wlFiltering.js
@@ -196,7 +196,7 @@ fs.readFile(filePath, 'utf8', async (err, data) => {
 
 	for (let i = 0; i < lines.length; i++) {
 
-		if (lines[i].match(/(ni)$/)) {
+		if (lines[i].match(/(ate)$/)) {
 
 			if (!resultsFound) {
 				process.stdout.write("\n");
